cbeb9c81a2a99ae4687dabebe8677f99d23d73bc
Mariastephan.J <63150376+Mariastephan@users.noreply.github.com>
2021-06-12 14:17:46 +0530

    Controller updated for service

---
+++ LaptopRentalApp/LaptopRental.API/Controllers/SignupController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace LaptopRental.API.Controllers
+{
+    public class SignupController : ApiController
+    {
+        private readonly SignupService signup;
+
+        public SignupController()
+        {
+            signup = new signup();
+        }
+        protected override void Dispose(bool disposing)
+        {
+            signup.Dispose();
+            base.Dispose(disposing);
+        }
+
+        public HttpResponseMessage Post(User obj)
+        {
+            if (ModelState.IsValid == false)
+                return BadRequest(ModelState);
+            try
+            {
+                bool saved = signup.Add(obj);
+            }
+}
+    }
