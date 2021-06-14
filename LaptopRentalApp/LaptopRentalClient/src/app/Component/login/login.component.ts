55bba1ba00c00e15a452d5b3966524e3cd3e6ec3
Priya Vijayakumar <priyavijayakumar01@gmail.com>
2021-06-14 08:46:03 +0530

    updated angular code

---
+++ LaptopRentalApp/LaptopRentalClient/src/app/Component/login/login.component.ts
@@ -0,0 +1,56 @@
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { LoginService } from 'src/app/Service/login.service';
+import { MenuComponent } from '../menu/menu.component';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css']
+})
+export class LoginComponent implements OnInit {
+
+  submitted : boolean =  false;
+  loginForm= new FormGroup({});
+  flag: any;
+  constructor(private ls : LoginService, 
+    private router: Router,
+    private menu : MenuComponent,
+    public formBuilder:FormBuilder) { }
+  ngOnInit(): void {
+    this.loginForm=this.formBuilder.group({
+      email: ['', [Validators.compose([Validators.required, Validators.pattern( "[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}"), Validators.minLength(1)])]],
+      password: ['', [Validators.required, Validators.minLength(8), Validators.pattern("^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*-]).{8,}$")]],
+       });
+  }
+  get f() { return this.loginForm.controls; }
+ Login() 
+ {
+  this.submitted = true;
+  const userDetails = {
+   email:this.loginForm.get("email")?.value,
+    password: this.loginForm.get('password')?.value
+  }
+  this.ls.Validate(userDetails)
+      .subscribe(
+        (res:any) => {
+          if(res) {
+            this.ls.isAuthenticated(true);
+            this.flag=res;
+                 this.check();
+            this.router.navigate(['/Companies']);  
+          }
+        },
+        (err:any) => {
+          this.ls.isAuthenticated(false);
+          alert('Invalid username/password');
+        }
+      );
+    this.loginForm.reset();
+  }
+
+  check(){
+this.menu.checkStatus(this.flag);
+  }
+}
---
+++ LaptopRentalApp/LaptopRentalClient/src/app/Types/User.ts
@@ -0,0 +1,13 @@
+export class User {
+    constructor (public UserId : Number,
+        public Name : String,
+        public Gender : String,
+        public DOB : String,
+        public Age : String,
+        public Location : String,
+        public PhoneNo : String,
+        public IdProof : String,
+        public Id_No : String,
+         public EmailId : String, 
+         public PassWord : String) {}
+}
