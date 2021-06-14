c127b8c537432b5d72c36c0a915def65a3930621
Mariastephan.J <63150376+Mariastephan@users.noreply.github.com>
2021-06-14 16:09:47 +0530

    Api for add devices Updated

---
+++ LaptopRentalApp/LaptopRental.BLL/Services/AddDeviceService.cs
@@ -0,0 +1,67 @@
+﻿using LaptopRental.DAL;
+using LaptopRental.DAL.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Common;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LaptopRental.BLL.Services
+{
+    public class AddDeviceService
+    {
+        public readonly LaptopRentalContext context;
+
+        public AddDeviceService()
+        {
+            context = new LaptopRentalContext();
+
+        }
+        public void Dispose()
+        {
+            context.Dispose();
+        }
+
+        public bool AddDevice(Device device)
+        {
+            try
+            {
+                var Obj = new Device();
+                if (Obj.DeviceId == 0)
+                {
+                    Obj.IMEINumber = device.IMEINumber;
+                    Obj.DeviceName = device.DeviceName;
+                    Obj.DeviceSpecification = device.DeviceSpecification;
+                    Obj.PreInstalledSoftware = device.PreInstalledSoftware;
+                    Obj.Ratings = device.Ratings;
+                    Obj.DeviceImage = device.DeviceImage;
+                    Obj.RentalAmount = device.RentalAmount;
+                    Obj.MaxRentalMonth = device.MaxRentalMonth;
+                    Obj.Interest = device.Interest;
+                    Obj.Status = device.Status;
+                    Obj.UserId_FK = device.UserId_FK;
+
+                    context.Devices.Add(Obj);
+                    int row = context.SaveChanges();
+                    if (row == 1)
+                    {
+                        return true;
+                    }
+                    else
+                    {
+                        return false;
+                    }
+                }
+            }
+            catch (DbException ex)
+            {
+                
+                throw new LaptopRentalException("Error in Writing", ex);
+            }
+            return false;
+        }
+        
+
+        }
+}
