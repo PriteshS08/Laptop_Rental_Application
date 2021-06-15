24d33474665697e683277982668259ca67589347
Mariastephan.J <63150376+Mariastephan@users.noreply.github.com>
2021-06-15 14:59:04 +0530

    updated db

--- LaptopRentalApp/LaptopRental.DAL/Migrations/202106150917344_LaptopRentalDB.cs
+++ LaptopRentalApp/LaptopRental.DAL/Migrations/202106150917344_LaptopRentalDB.cs
@@ -3,7 +3,7 @@
     using System;
     using System.Data.Entity.Migrations;
     
-    public partial class db : DbMigration
+    public partial class LaptopRentalDB : DbMigration
     {
         public override void Up()
         {
@@ -28,12 +28,31 @@
                 .ForeignKey("dbo.Users", t => t.UserId_FK, cascadeDelete: true)
                 .Index(t => t.UserId_FK);
             
+            CreateTable(
+                "dbo.Users",
+                c => new
+                    {
+                        UserId = c.Int(nullable: false, identity: true),
+                        Name = c.String(nullable: false),
+                        Gender = c.String(nullable: false),
+                        DOB = c.DateTime(nullable: false),
+                        Age = c.Int(nullable: false),
+                        Location = c.String(nullable: false),
+                        PhoneNO = c.String(nullable: false),
+                        IdProof = c.String(nullable: false),
+                        Id_No = c.String(nullable: false),
+                        EmailId = c.String(nullable: false),
+                        PassWord = c.String(nullable: false),
+                    })
+                .PrimaryKey(t => t.UserId);
+            
         }
         
         public override void Down()
         {
             DropForeignKey("dbo.Devices", "UserId_FK", "dbo.Users");
             DropIndex("dbo.Devices", new[] { "UserId_FK" });
+            DropTable("dbo.Users");
             DropTable("dbo.Devices");
         }
     }
