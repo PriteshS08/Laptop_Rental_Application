fa999fb186b3f7aaae3f5eecfd270e616402ff79
Priya Vijayakumar <priyavijayakumar01@gmail.com>
2021-06-15 23:54:06 +0530

    updated code

--- LaptopRentalApp/LaptopRentalClient/src/app/app-routing.module.ts
+++ LaptopRentalApp/LaptopRentalClient/src/app/app-routing.module.ts
@@ -5,6 +5,8 @@ import { LoginComponent } from './Component/login/login.component';
 import { SignupComponent } from './Component/signup/signup.component';
 import { CustomerUIComponent } from './Component/customer-ui/customer-ui.component';
 import { LenderUIComponent } from './Component/lender-ui/lender-ui.component';
+import { EditDeviceComponent } from './Component/edit-device/edit-device.component';
+import { AddDeviceComponent } from './Component/add-device/add-device.component';
 
 const routes: Routes = [
     {path : 'login' , component : LoginComponent},
@@ -12,6 +14,8 @@ const routes: Routes = [
     {path : 'customer' , component : CustomerUIComponent},
     {path : 'lender' , component : LenderUIComponent},
     {path : 'home' , component : HomeComponent},
+    {path : 'lender' , component : EditDeviceComponent},
+    {path : 'home' , component : AddDeviceComponent},
 ];
 
 @NgModule({
