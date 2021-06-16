9ec4bb42efa0191f5e5693f074f74da37255d537
Mariastephan.J <63150376+Mariastephan@users.noreply.github.com>
2021-06-16 20:45:05 +0530

    Request Database updated

---
+++ LaptopRentalApp/LaptopRental.DAL/Models/Request.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LaptopRental.DAL.Models
+{
+    public class Request
+    {
+        [Key]
+        public int RequestId { get; set; }
+
+        public DateTime RequestDate { get; set; }
+
+        public DateTime FromDate { get; set; }
+
+        public DateTime ToDate { get; set; }
+
+        public string RequestStatus { get; set; }
+
+
+        [ForeignKey("Device")]
+        public int DeviceId_FK { get; set; }
+        public Device Device { get; set; }
+
+    }
+}
