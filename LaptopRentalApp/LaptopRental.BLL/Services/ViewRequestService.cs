c7f6a20ab53b66c9edecce3729b9eaa8e1d8a124
Riyas Ahamed A <riyasahamed240919@gmail.com>
2021-06-20 15:35:17 +0530

    Updated Code

--- LaptopRentalApp/LaptopRental.BLL/Services/ViewRequestService.cs
+++ LaptopRentalApp/LaptopRental.BLL/Services/ViewRequestService.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using LaptopRental.DAL;
+using LaptopRental.DAL.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,8 +8,19 @@ using System.Threading.Tasks;
 
 namespace LaptopRental.BLL.Services
 {
+
+   
     public class ViewRequestService
     {
+        private readonly LaptopRentalContext context;
+        public ViewRequestService()
+        {
+            context = new LaptopRentalContext();
+        }
 
+        public Request GetRequest()
+        {
+
+        }
     }
 }
