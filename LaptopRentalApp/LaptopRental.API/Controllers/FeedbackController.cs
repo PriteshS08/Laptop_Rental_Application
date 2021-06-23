34dca15e3a8eedf313cd55168019301f388e7788
Mariastephan.J <63150376+Mariastephan@users.noreply.github.com>
2021-06-23 23:21:36 +0530

    Rented Devices

--- LaptopRentalApp/LaptopRental.API/Controllers/FeedbackController.cs
+++ LaptopRentalApp/LaptopRental.API/Controllers/FeedbackController.cs
@@ -1,62 +1,62 @@
-﻿using LaptopRental.BLL;
-using LaptopRental.DAL;
-using LaptopRental.DAL.Models;
-using LaptopRental.BLL.Services;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Net;
-using System.Net.Http;
-using System.Web.Http;
+﻿//using LaptopRental.BLL;
+//using LaptopRental.DAL;
+//using LaptopRental.DAL.Models;
+//using LaptopRental.BLL.Services;
+//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Net;
+//using System.Net.Http;
+//using System.Web.Http;
 
-namespace LaptopRental.API.Controllers
-{
-    public class FeedbackController : ApiController
-    {
-        LaptopRentalContext context = new LaptopRentalContext();
-
-
+//namespace LaptopRental.API.Controllers
+//{
+//    public class FeedbackController : ApiController
+//    {
+//        LaptopRentalContext context = new LaptopRentalContext();
 
 
-        private readonly FeedbackService feedbackService;
-
-        public FeedbackController()
-        {
-            feedbackService = new FeedbackService();
-        }
 
 
-        /// <summary>
-        /// Post method for the SignUp page
-        /// </summary>
-        /// <param name="model"></param>
-        /// <returns> created if User details saved to the database </returns>
+//        private readonly FeedbackService feedbackService;
 
-        [HttpPost]
+//        public FeedbackController()
+//        {
+//            feedbackService = new FeedbackService();
+//        }
 
-        public IHttpActionResult AddPost(Feedback use)
 
-        {
-            try
-            {
-                if (!ModelState.IsValid)
-                    return BadRequest(ModelState);
+//        /// <summary>
+//        /// Post method for the SignUp page
+//        /// </summary>
+//        /// <param name="model"></param>
+//        /// <returns> created if User details saved to the database </returns>
 
-                var Obj = new Feedback();
-                Obj.Comment = use.Comment;
-                Obj.Ratings = use.Ratings;
-                Obj.FeedBackDate = use.FeedBackDate;
-                Obj.UserId = use.UserId;
+//        [HttpPost]
 
-                var isadded = feedbackService.AddRating(Obj);
-                if (isadded)
-                    return StatusCode(HttpStatusCode.Created);
-                return BadRequest("failed");
-            }
-            catch (LaptopRentalException ex)
-            {
-                return InternalServerError(ex);
-            }
-        }
-    }
-}
+//        public IHttpActionResult AddPost(Feedback use)
+
+//        {
+//            try
+//            {
+//                if (!ModelState.IsValid)
+//                    return BadRequest(ModelState);
+
+//                var Obj = new Feedback();
+//                Obj.Comment = use.Comment;
+//                Obj.Ratings = use.Ratings;
+//                Obj.FeedBackDate = use.FeedBackDate;
+//                Obj.UserId = use.UserId;
+
+//                var isadded = feedbackService.AddRating(Obj);
+//                if (isadded)
+//                    return StatusCode(HttpStatusCode.Created);
+//                return BadRequest("failed");
+//            }
+//            catch (LaptopRentalException ex)
+//            {
+//                return InternalServerError(ex);
+//            }
+//        }
+//    }
+//}
---
+++ LaptopRentalApp/LaptopRental.BLL/Services/RentedDeviceService.cs
@@ -0,0 +1,53 @@
+﻿using LaptopRental.DAL;
+using LaptopRental.DAL.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Common;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LaptopRental.BLL.Services
+{
+   public class RentedDeviceService
+    {
+        public readonly LaptopRentalContext context;
+        public RentedDeviceService()
+        {
+            context = new LaptopRentalContext();
+        }
+        public void Dispose()
+        {
+            context.Dispose();
+        }
+        public List<Request> GetRentedDevices(int UserId)
+        {
+            try
+            {
+                List<Request> requests = context.Requests.ToList();
+                List<Device> devices = context.Devices.ToList();
+
+                var query = from req in requests
+                            join dev in devices on req.DeviceId_FK equals dev.DeviceId
+                            where req.UserId_FK == UserId && req.RequestStatus.ToLower() == "rented" && dev.Status.ToLower() == "rented"
+                            select req;
+
+                if (query != null)
+                {
+                    return query.ToList();
+
+                }
+                return new List<Request>();
+            }
+            catch (DbException ex)
+            {
+                throw new LaptopRentalException("Database error fetching data", ex);
+            }
+            catch (Exception ex)
+            {
+                throw new LaptopRentalException("Unknown error while fetching data", ex);
+            }
+        }
+       
+    }
+}
