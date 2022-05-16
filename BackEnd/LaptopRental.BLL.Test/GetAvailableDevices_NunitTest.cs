546c0b2f6d186741a65dc6f94055c6cd489a5ae4
PriteshSurale <p.l.surale@gmail.com>
2022-05-17 02:26:45 +0530

    Issue#3 Resolved

--- BackEnd/LaptopRental.BLL.Test/GetAvailableDevices_NunitTest.cs
+++ BackEnd/LaptopRental.BLL.Test/GetAvailableDevices_NunitTest.cs
@@ -1,24 +1,62 @@
 ﻿using System;
 using NUnit.Framework;
 using LaptopRental.BLL.Services;
-
+using LaptopRental.BLL;
 namespace LaptopRental.BLL.Test
 {
     [TestFixture]
     class GetAvailableDevices_NunitTest
     {
+        private BrowserCatalogueService _BCS;
+        private FetchingCountOfAvailableDevice _FADC;
+
+        [SetUp]
+        public void setup()
+        {
+            _BCS = new BrowserCatalogueService();
+            _FADC = new FetchingCountOfAvailableDevice();
+        }
+        
         [Test]
         public void GetAvailableDevices_Test()
         {
             //Arrange 
-            var BCS = new BrowserCatalogueService();
+            //var BCS = new BrowserCatalogueService();
            
 
             //Act
-            var AvailableDevice = BCS.GetAvailableDevices();
+            var AvailableDevice = _BCS.GetAvailableDevices();
 
             //Assert
             Assert.That(AvailableDevice.Count, Is.EqualTo(8));
         }
+
+
+        [Test]
+        public void FetchingAvailableDevicesCount_Test()
+        {
+            //Arrange 
+            //var FADC = new FetchingCountOfAvailableDevice();
+
+
+            //Act
+            var count = _FADC.AvailableDevice();
+
+            //Assert
+            Assert.That(count, Is.EqualTo(8));
+        }
+
+        [Test]
+        public void GetAvailableDevices_TestingBy_FetchingAvailableDevicesCount()
+        {
+            var AvailableDevice = _BCS.GetAvailableDevices();
+            var count = _FADC.AvailableDevice();
+
+            Assert.AreEqual(AvailableDevice.Count,count);
+
+        }
+
+
+
     }
 }
---
+++ BackEnd/LaptopRental.BLL/FetchingCountOfAvailableDevice.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+namespace LaptopRental.BLL
+{
+    public class FetchingCountOfAvailableDevice
+    {
+        public int AvailableDevice()
+        {
+            var con = @"data source=(localdb)\MSSQLLocalDB;initial catalog=LaptopRental.DAL.LaptopRentalContext;integrated security=True";
+            SqlConnection connection = new SqlConnection(con);
+            var query = "select count(*) from Devices where Status='Available'";
+            var command = new SqlCommand(query, connection);
+            connection.Open();
+            var num = command.ExecuteScalar();
+            connection.Close();
+            return (int)num;
+        }
+    }
+}
