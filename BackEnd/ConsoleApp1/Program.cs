a58cc45c809b5ed08a1f0e9f0ebf81047d5b6fca
PriteshSurale <p.l.surale@gmail.com>
2022-05-17 02:00:41 +0530

    Issue#3 Resolved

---
+++ BackEnd/ConsoleApp1/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using LaptopRental.BLL.Services;
+using System.Data.SqlClient;
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var bcs = new BrowserCatalogueService();
+            var k = bcs.GetAvailableDevices();
+            Console.WriteLine(k.Count);
+
+
+            var P = new Program();
+
+            var fun = P.AvailableDevice();
+            Console.WriteLine(fun);
+            
+            Console.ReadKey();
+
+
+
+
+        }
+        public int AvailableDevice()
+        {
+            var con = @"data source=(localdb)\MSSQLLocalDB;initial catalog=LaptopRental.DAL.LaptopRentalContext;integrated security=True";
+            SqlConnection connection = new SqlConnection(con);
+            var query = "select count(*) from Devices where Status='Available'";
+            var command = new SqlCommand(query, connection);
+            connection.Open();
+            var num = command.ExecuteScalar();
+            connection.Close();
+            return (int)num;
+        }
+
+    }
+}
---
+++ BackEnd/LaptopRental.BLL.Test/GetAvailableDevices_NunitTest.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using NUnit.Framework;
+using LaptopRental.BLL.Services;
+
+namespace LaptopRental.BLL.Test
+{
+    [TestFixture]
+    class GetAvailableDevices_NunitTest
+    {
+        [Test]
+        public void GetAvailableDevices_Test()
+        {
+            //Arrange 
+            var BCS = new BrowserCatalogueService();
+           
+
+            //Act
+            var AvailableDevice = BCS.GetAvailableDevices();
+
+            //Assert
+            Assert.That(AvailableDevice.Count, Is.EqualTo(8));
+        }
+    }
+}
